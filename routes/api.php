f8e703948bfe40bb92cd20ae24680d1ecbeed9aa
Echezona-Mbah <cclassico998@gmail.com>
2025-04-17 21:50:03 +0100

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('auth/register', [RegisterController::class, 'register']);
+Route::post('auth/verify-email/{email}', [RegisterController::class, 'verifyEmail']);
+Route::post('auth/new-email-otp/{email}', [RegisterController::class, 'verifyEmailOtp']);
+Route::post('auth/login', [LoginController::class, 'login']);
+
+
+Route::group(['middleware'=> ['auth:sanctum']],function(){
+    Route::get('/users', [RegisterController::class, 'getAllUsers']);
+    Route::delete('/deleteUser/{email}', [RegisterController::class, 'deleteUser']);
+
+
+
+
+
+});
+
